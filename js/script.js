b0a804235ae2d5c05e6615a44c9fd11264b275c6
ronentgreat <ronenh53@gmail.com>
2022-06-14 11:15:42 +0000

    added carousel

--- js/script.js
+++ js/script.js
@@ -6,3 +6,31 @@ btn.addEventListener('click', () => {
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
 })
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+
+function plusSlides(n){
+  showSlides(slideIndex += n);
+}
+
+// Thumbnial image controls
+function currentSlide(n){
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length){slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for(i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
